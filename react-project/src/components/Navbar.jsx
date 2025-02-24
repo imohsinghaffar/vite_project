5e5d853f97182e53bb9fddf7f750e937da1a2425
Mohsin Ghaffar <mohsinghaffar546@gmail.com>
2025-02-24 14:56:33 +0100

    tested app.jsx file

--- react-project/src/components/Navbar.jsx
+++ react-project/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({title}) => {
     <>
 
 
-<nav className="bg-white dark:bg-gray-900 fixed shadow-lg w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+<nav className="bg-white mb-32 dark:bg-gray-900 fixed shadow-lg w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{title}</span>
